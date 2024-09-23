3f800173bf95ecae9076cf3918b53c510d7ba852
Eyjolfur Gudjonsson <eyjolfur.gudjonsson@svenskaspel.se>
2024-09-24 00:20:38 +0200

    fixes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const canvas = document.getElementById("tutorial");
 const ctx = canvas.getContext("2d", { willReadFrequently: true });
 let frames = 0;
 const degInRad = Math.PI / 180;
+const radInDeg = 180 / Math.PI;
 
 const CANVAS = {
   WIDTH: null,
@@ -13,15 +14,30 @@ const CANVAS = {
   HEIGHT2: null
 };
 
+const colors = {
+  RED: "rgba(255, 0, 0, 1)",
+  RED_DARK: "rgba(150, 0, 0, 1)",
+  GREEN: "rgba(0, 255, 0, 1)",
+  BLUE: "rgba(0, 0, 255, 1)",
+  YELLOW: "rgba(255, 255, 0, 1)",
+  CEILING: "rgba(59,59, 59, 1)",
+  FLOOR: "rgba(115, 115, 115, 1)"
+
+};
+
 
 const player = {
   x: 300,
   y: 300,
   dx: 5,
   dy: 0,
-  z: 10,
   angle: 0.00000001,
-  color:"rgba(255, 255, 0, 1)"
+  color: colors.YELLOW,
+  checkCollision: function(x,y){
+    const mapX = Math.floor(x / map.gridSize);
+    const mapY = Math.floor(y / map.gridSize);
+    return map.map[mapX + map.y * mapY] === 1;
+  }
 }; 
 
 const map = {
@@ -48,51 +64,27 @@ const projection3D = (ray,i,angle) => {
   const DRAW_WIDTH = 8;
   const {mapHeight} = map; 
   const rectifiedDistance = ray.distance * Math.cos(player.angle - angle);
-  console.log('ray.angle',ray.angle, 'player.angle',player.angle);
-  console.log('dist',ray.distance);
+
   // scaling to get a usable height hand tuned
   const height = Math.min((mapHeight * 64) / rectifiedDistance, mapHeight);
   const top = CANVAS.HEIGHT2 - height / 2;
-  const bottom = CANVAS.HEIGHT2 + height / 2;
-  console.log('top',top,'bottom',bottom); 
-  console.log('mapHeight',mapHeight,'height',height);
-  ctx.fillStyle = ray.isVertical ?  "rgba(255, 0, 0, 1)" : 'rgba(150, 0, 0, 1)';
-  ctx.fillRect(CANVAS.WIDTH2 + i * DRAW_WIDTH, top, 8 * DRAW_WIDTH, height);
+
+  ctx.fillStyle = ray.isVertical ?  colors.RED : colors.RED_DARK;
+  ctx.fillRect(CANVAS.WIDTH2 + i * DRAW_WIDTH, top, DRAW_WIDTH, height);
 };
 
 const drawCeiling = () => {
   const fillStyle = ctx.fillStyle;
-  ctx.fillStyle = "rgba(59,59, 59, 1)"; // #3B3B3B
+  ctx.fillStyle = colors.CEILING; // #3B3B3B
   ctx.fillRect(CANVAS.WIDTH2, 0, CANVAS.WIDTH, CANVAS.HEIGHT2); // #737373
   ctx.fillStyle = fillStyle;
 };
 const drawFloor = () => {
   const fillStyle = ctx.fillStyle;
-  ctx.fillStyle = "rgba(115, 115, 115, 1)";
+  ctx.fillStyle = colors.FLOOR;
   ctx.fillRect(CANVAS.WIDTH2, CANVAS.HEIGHT / 2, CANVAS.WIDTH, CANVAS.HEIGHT);
   ctx.fillStyle = fillStyle;
 };
-
-/*
-const drawMap2d = () => {
-  ctx.fillRect(player.x+10,player.y,4,4);
-  const {gridSize,wallColor,floorColor} = map;
-  console.log(gridSize,wallColor,floorColor)
-  map.map.forEach((wall,index)=>{
-    //console.log(wall)
-    const x = index%map.x
-    const y = Math.floor(index/map.y)
-    ctx.fillColor = wall ? wallColor : floorColor
-    ctx.fillRect(x, y, gridSize, gridSize);
-  })
-}
-*/
-/*
-const wall = {
-  x: Array(4),
-  y: Array(4),
-  z: Array(4)
-}*/
 
 const handleKey = (e,set) => {
   pushKeys[e.key] = set;
@@ -144,7 +136,7 @@ const calcRays3D = (angle) => {
     }
     // pythagorean theorem
     distance = Math.sqrt(Math.pow(ray.x - player.x, 2) + Math.pow(ray.y - player.y, 2));
-    return {x: Math.round(ray.x), y: Math.round(ray.y), hitDepth, distance, isVertical};
+    return {...ray, x: Math.round(ray.x), y: Math.round(ray.y), hitDepth, distance, isVertical};
   };
 
   
@@ -155,14 +147,14 @@ const calcRays3D = (angle) => {
 
   const {gridSize} = map;
   // looking up
-  if( player.angle > Math.PI){
+  if( angle > Math.PI){
     ray.y = Math.floor(player.y / gridSize) * gridSize - 0.0001; // this should never be 0
     ray.x = (player.y - ray.y) * atanRayAngle + player.x;
     yOffset = -gridSize;
     xOffset = -yOffset * atanRayAngle;
 
     // looking down
-  } else if(player.angle < Math.PI && player.angle > 0){
+  }  else if(angle < Math.PI && angle > 0){
     ray.y = Math.floor(player.y / gridSize) * gridSize + gridSize;
     ray.x = ((player.y - ray.y) * atanRayAngle + player.x);
     yOffset = gridSize;
@@ -180,7 +172,7 @@ const calcRays3D = (angle) => {
   const nTan = -Math.tan(ray.angle);
 
   // looking left
-  if( player.angle > Math.PI / 2 && player.angle < 3 * Math.PI / 2){
+  if( angle > Math.PI / 2 && angle < 3 * Math.PI / 2){
     ray.x =   Math.floor(player.x / gridSize) * gridSize - 0.0001; // this should never be 0
     ray.y = (player.x - ray.x) * nTan + player.y;
 
@@ -188,7 +180,7 @@ const calcRays3D = (angle) => {
     yOffset = -xOffset * nTan;
 
     // looking right
-  } else if(player.angle < Math.PI / 2 || player.angle > 3 * Math.PI / 2){
+  } else if(angle < Math.PI / 2 || angle > 3 * Math.PI / 2){
     ray.x = Math.floor(player.x / gridSize) * gridSize + gridSize;
     ray.y = ((player.x - ray.x) * nTan + player.y);
 
@@ -229,7 +221,8 @@ const draw = () => {
 
 
 
-
+  const prevX = player.x;
+  const prevY = player.y;
 
   switch(true){
   //   case pushKeys.w && pushKeys.a:
@@ -273,14 +266,22 @@ const draw = () => {
 
   }
 
-  // ctx.clearRect(0,0,WIDTH,HEIGHT)
+  // is player inside a wall
+  if(player.checkCollision(player.x,player.y)){
+    // the player will be inside a wall
+    // check if the player should wallslide
+    if(!player.checkCollision(player.x,prevY)){
+      player.y = prevY;
+    } else if (!player.checkCollision(prevX,player.y)){
+      player.x = prevX;
+    } else {
+      player.x = prevX;
+      player.y = prevY; 
+    }
+  }
+
   ctx.fillStyle = "rgba(100, 100, 100, 1)";
   ctx.fillRect(0,0,WIDTH,HEIGHT);
-
-
-  // const dx=sin(player.angle)*10;
-  // const dy=cos(player.angle)*10;
-
 
   ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
   ctx.fillRect(frames++ % WIDTH, 0, 4, 4);
@@ -295,15 +296,6 @@ const draw = () => {
     ctx.fillRect(x * cellSize + 1, y * cellSize + 1, cellSize - 1, cellSize - 1);
   });
 
-  const drawRays3D = (ray) => {
-    ctx.beginPath();
-    ctx.moveTo(playerXRound, playerYRound);
-    ctx.lineTo(ray.x, ray.y);
-    ctx.stroke();
-  };
-
-
-
   // Draw player
   const playerXRound = Math.round(player.x);
   const playerYRound = Math.round(player.y);
@@ -314,55 +306,47 @@ const draw = () => {
   ctx.lineTo(player.x + Math.floor(player.dx * 5), player.y + Math.floor(player.dy * 5));
   ctx.stroke();
 
-
-
+  const drawRays3D = (ray) => {
+    ctx.beginPath();
+    ctx.moveTo(playerXRound, playerYRound);
+    ctx.lineTo(ray.x, ray.y);
+    ctx.stroke();
+  };
 
   drawCeiling();
   drawFloor();
 
-  const rayArray = Array.from({length:64}, (_ ,i)=>{
+  Array.from({length:64}, (_ ,i)=>{
     let angle = player.angle - 30 * degInRad + i * degInRad;
     if (angle < 0) {
       angle += 2 * Math.PI;
     } else if (angle > 2 * Math.PI) {
       angle -= 2 * Math.PI;
     }
-    return angle;
-  });
-
-  rayArray.forEach((angle,i)=>{
+    if(i === 0){
+      console.log('angle',angle * radInDeg);
+      ctx.beginPath();
+      ctx.strokeStyle = "rgba(0, 255, 0, 1)";
+      ctx.moveTo(playerXRound, playerYRound);
+      const lineX = 1000;
+      const lineY = 0;
+      ctx.lineTo( playerXRound + lineX * Math.cos(angle) - lineY * Math.sin(angle), playerYRound + lineX * Math.sin(angle)  + lineY * Math.sin(angle));
+      ctx.stroke();
+      
+    } 
+    ctx.strokeStyle = player.color;
     const ray = calcRays3D(angle);
     drawRays3D(ray);
-    console.log('ray',ray);
+    // console.log('ray',ray);
     projection3D(ray,i,angle);
   });
 
-
-
-
-  // ctx.beginPath();
-  // ctx.strokeStyle = "rgba(0, 255, 0, 0.5)";
-  // ctx.moveTo(playerXRound, playerYRound);
-  // ctx.lineTo(ray.a, ray.b);
-  // ctx.stroke();
-  // ctx.strokeStyle = player.color;
-
+  ctx.strokeStyle = player.color;
+  ctx.fillStyle = player.color;
   ctx.fillRect(player.x - 2,player.y - 2,5,5);
 
   const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
-
-  // const data = imageData.data;
-  // console.log(data.length)
-  // console.log(imageData)
-  // for (let i = 0; i < data.length-frames++; i += 4) {
-  //   data[i] = 0; // red
-  //   data[i + 1] = 0; // green
-  //   data[i + 2] = 0; // blue
-  //   data[i + 3] = 255; // alpha
-  //   ctx.putImageData(imageData, 0, 0);
-  // }
-  // console.log(imageData)
   ctx.putImageData(imageData, 0, 0);
 };
 
